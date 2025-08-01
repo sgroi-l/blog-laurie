abd7699dea73d97bb133057b00cb41b3fc9c9778
Laurie <laurie@agile.coop>
2025-08-01 15:29:32 +0100

    Reverse order

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,23 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <h2 className="mb-0">
-            <Link href={post.slug}>{post.title}</Link>
-          </h2>
-          <span className="text-xs">
-            {new Date(post.date).toLocaleDateString("en-GB", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
-          </span>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
+      {allPosts
+        .map((post) => (
+          <article key={post._id}>
+            <h2 className="mb-0">
+              <Link href={post.slug}>{post.title}</Link>
+            </h2>
+            <span className="text-xs">
+              {new Date(post.date).toLocaleDateString("en-GB", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </span>
+            {post.description && <p>{post.description}</p>}
+          </article>
+        ))
+        .reverse()}
     </div>
   );
 }
